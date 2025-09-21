cb7825f0e27cba7d0be373ca8bb16816805e089b
Antonio-Alisson-Teixeira-Cruz <ravenjouki@gmail.com>
2025-09-21 01:32:25 +0000

    up

--- class/Livro.js
+++ class/Livro.js
@@ -1,9 +1,19 @@
 class Livro {
-    constructor(titulo, autora, anoPublicacao, paginas, exemplares) {
+    constructor(titulo, autor, genero, anoPublicacao, paginas, exemplares) {
         this.titulo = titulo
-        this.autora = autora
+        this.autor = autor
+        this.genero = genero
         this.anoPublicacao = anoPublicacao
         this.paginas = paginas
         this.exemplares = exemplares
+    }
+
+    mostrarInformacoes() {
+        return `Título: ${this.titulo}
+Autor: ${this.autor}
+genero: ${this.genero}
+Ano de Publicação: ${this.anoPublicacao}
+Páginas: ${this.paginas}
+Exemplares disponiveis: ${this.exemplares}`
     }
 }
