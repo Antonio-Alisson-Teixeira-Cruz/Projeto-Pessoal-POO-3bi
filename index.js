536f40c2ae2d7fe0a3c6c5e466a4bf08b78baa58
Antonio-Alisson-Teixeira-Cruz <ravenjouki@gmail.com>
2025-09-22 14:53:54 +0000

    up

--- index.js
+++ index.js
@@ -2,16 +2,41 @@ const prompt = require("prompt-sync")()
 let continuar = true
 let menuAlunoContinuar = true
 
-function menuAluno(){
-    while(menuAlunoContinuar){
-        let turma = prompt('De que turma você é? ')
-        
-    }
-}
 
 
 
 while(continuar) {
+    function menuAluno(){
+        while(menuAlunoContinuar){
+            let turma = prompt('De que turma você é? ')
+            
+            const aluno = new Aluno(nome, idade, classe)
+
+            let menu = prompt(`Menu do aluno
+O que deseja fazer?
+1 - Acessar painel.
+2 - Verificar informações.
+3 - Pegar livro.
+4 - Devolver livro.`)
+
+            switch (menu) {
+                case '1':
+                    aluno.acessoPainel()
+                    break;
+            
+                case '2':
+                    aluno.mostrarInformacoes()
+                    break
+
+                case '3':
+                    let livro = prompt("Que livro deseja pegar emprestado?")
+
+                default:
+                    console.log('Dado inválido!')
+                    break;
+            }
+        }
+    }
     let nome = prompt("Digite seu nome: ")
     let idade = prompt("Digite sua idade: ")
     let classe = prompt("Você é: \n1 - Aluno \n2 - Bibliotecário \nobs.: Use apenas números")
